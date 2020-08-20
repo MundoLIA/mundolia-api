7b5a7050ac9350660658045557dcb55bee1b7c87
Luis Cruz <antonio2120@hotmail.com>
2020-08-20 12:03:27 -0500

    Merge Passport Models

--- routes/api.php
+++ routes/api.php
@@ -18,29 +18,31 @@ Route::post('register', 'API\UserController@register');
 
 Route::group(['middleware' => 'auth:api'], function(){
     Route::post('access-token', 'API\UserController@accessToken');
+
+
+    Route::get('escuelas', 'SchoolController@index');
+    Route::get('escuelas/{id}', 'SchoolController@show');
+    Route::post('escuelas', 'SchoolController@store');
+    Route::put('escuelas/{id}', 'SchoolController@update');
+    Route::delete('escuelas/{id}', 'SchoolController@destroy');
+
+    Route::get('usuarios', 'UserController@index');
+    Route::get('usuarios/{uuid}', ['as' => 'usuarios/{uuid}', 'uses'=>'UserController@show']);
+    Route::post('usuarios', 'UserController@store');
+    Route::put('usuarios/{uuid}', 'UserController@update');
+    Route::delete('usuarios/{uuid}', 'UserController@destroy');
+
+    Route::get('tipolicencias', 'LicenseTypeController@index');
+    Route::get('tipolicencias/{id}', 'LicenseTypeController@show');
+    Route::post('tipolicencias', 'LicenseTypeController@store');
+    Route::put('tipolicencias/{id}', 'LicenseTypeController@update');
+    Route::delete('tipolicencias/{id}', 'LicenseTypeController@destroy');
+
+    Route::get('licencias', 'LicenseController@index');
+    Route::get('licencias/{id}', 'LicenseController@show');
+    Route::post('licencias', 'LicenseController@store');
+    Route::put('licencias/{id}', 'LicenseController@update');
+    Route::delete('licencias/{id}', 'LicenseController@destroy');
 });
 
 
-Route::get('escuelas', 'SchoolController@index');
-Route::get('escuelas/{id}', 'SchoolController@show');
-Route::post('escuelas', 'SchoolController@store');
-Route::put('escuelas/{id}', 'SchoolController@update');
-Route::delete('escuelas/{id}', 'SchoolController@destroy');
-
-Route::get('usuarios', 'UserController@index');
-Route::get('usuarios/{uuid}', ['as' => 'usuarios/{uuid}', 'uses'=>'UserController@show']);
-Route::post('usuarios', 'UserController@store');
-Route::put('usuarios/{uuid}', 'UserController@update');
-Route::delete('usuarios/{uuid}', 'UserController@destroy');
-
-Route::get('tipolicencias', 'LicenseTypeController@index');
-Route::get('tipolicencias/{id}', 'LicenseTypeController@show');
-Route::post('tipolicencias', 'LicenseTypeController@store');
-Route::put('tipolicencias/{id}', 'LicenseTypeController@update');
-Route::delete('tipolicencias/{id}', 'LicenseTypeController@destroy');
-
-Route::get('licencias', 'LicenseController@index');
-Route::get('licencias/{id}', 'LicenseController@show');
-Route::post('licencias', 'LicenseController@store');
-Route::put('licencias/{id}', 'LicenseController@update');
-Route::delete('licencias/{id}', 'LicenseController@destroy');
