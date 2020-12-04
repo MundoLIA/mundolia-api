1a8f0ed9264488b77214d865a8563c4945723dbf
Dylanmars <nivoastro@gmail.com>
2020-12-04 01:11:25 -0600

    New group name validation

--- app/Http/Controllers/SyncGroupComunnityController.php
+++ app/Http/Controllers/SyncGroupComunnityController.php
@@ -34,89 +34,93 @@ class SyncGroupComunnityController extends ApiController
 
                 $syncSchool = $obj;
 
-                // DATA PHPFOX_PAGES TABLE
-                $data = ([
-                    'app_id' => 0,
-                    'view_id' => 0,
-                    'type_id' => 6,
-                    "category_id" => 0,
-                    "user_id" => 20,
-                    "title" => $syncSchool->name,
-                    "reg_method" => 2,
-                    "landing_page" => null,
-                    "time_stamp" => Carbon::now()->timestamp,
-                    "image_path" => null,
-                    "is_featured" => 0,
-                    "is_sponsor" => 0,
-                    "image_server_id" => 0,
-                    "total_like" => 1,
-                    "total_dislike" => 0,
-                    "total_comment" => 0,
-                    "privacy" => 0,
-                    "designer_style_id" => 0,
-                    "cover_photo_id" => null,
-                    "cover_photo_position" => null,
-                    "location_latitude" => null,
-                    "location_longitude" => null,
-                    "location_name" => null,
-                    "use_timeline" => 0,
-                    "item_type" => 1
-                ]);
-
-                $group = SyncGroupComunnity::create($data);
-
-                // DATA PHPFOX_PAGES TABLE
-                $dataText = [
-                    'page_id' => $group->page_id,
-                    'text' => null,
-                    'text_parsed' => null
-                ];
-
-                $pageText = PhpFoxPageText::create($dataText);
-
-                $dataUserCommunity = ([
-                    'profile_page_id' => $group->page_id,
-                    'user_group_id' => 2,
-                    'view_id' => 7,
-                    'full_name' => $group->title,
-                    'joined' => Carbon::now()->timestamp
-                ]);
-
-                $userCommunity = UserCommunity::create($dataUserCommunity);
-
-                // List all user of school to register on group phpfox
-                $userList = User::where([
-                    ['school_id', '=', $syncSchool->id],
-                    ['active_phpfox', '!=', 0]
-                ])->get();
-
-                $t = 0;
-
-                foreach ($userList as $user) {
-
-                    $userId = $user->id;
-
-                    // DATA PHPFOX_LIKE TABLE
-                    $dataLike = ([
-                        "type_id" => "groups",
-                        "item_id" => $group->page_id,
-                        "user_id" => $user->active_phpfox,
-                        "feed_table" => "feed",
-                        "time_stamp" => Carbon::now()->timestamp
+                if (SyncGroupComunnity::where([['title', '=', $syncSchool->name]])) {
+                    $count[$c++] = array('error' => 'El nombre de grupo ya existe');
+                } else {
+                    // DATA PHPFOX_PAGES TABLE
+                    $data = ([
+                        'app_id' => 0,
+                        'view_id' => 0,
+                        'type_id' => 6,
+                        "category_id" => 0,
+                        "user_id" => 20,
+                        "title" => $syncSchool->name,
+                        "reg_method" => 2,
+                        "landing_page" => null,
+                        "time_stamp" => Carbon::now()->timestamp,
+                        "image_path" => null,
+                        "is_featured" => 0,
+                        "is_sponsor" => 0,
+                        "image_server_id" => 0,
+                        "total_like" => 1,
+                        "total_dislike" => 0,
+                        "total_comment" => 0,
+                        "privacy" => 0,
+                        "designer_style_id" => 0,
+                        "cover_photo_id" => null,
+                        "cover_photo_position" => null,
+                        "location_latitude" => null,
+                        "location_longitude" => null,
+                        "location_name" => null,
+                        "use_timeline" => 0,
+                        "item_type" => 1
                     ]);
 
-                    $userLike = LikeUserGroup::create($dataLike);
+                    $group = SyncGroupComunnity::create($data);
 
-                    $dataGroup = ([
-                        'user_id' => $userId,
-                        'school_id' => $syncSchool->id,
-                        'group_id_community' => $group->page_id,
-                        'group_id_academy' => 0
+                    // DATA PHPFOX_PAGES TABLE
+                    $dataText = [
+                        'page_id' => $group->page_id,
+                        'text' => null,
+                        'text_parsed' => null
+                    ];
+
+                    $pageText = PhpFoxPageText::create($dataText);
+
+                    $dataUserCommunity = ([
+                        'profile_page_id' => $group->page_id,
+                        'user_group_id' => 2,
+                        'view_id' => 7,
+                        'full_name' => $group->title,
+                        'joined' => Carbon::now()->timestamp
                     ]);
 
-                    $groupCreated = GroupUserEnrollment::create($dataGroup);
-                    $t++;
-                    //$count[$t++] = [$userLike, $groupCreated];
+                    $userCommunity = UserCommunity::create($dataUserCommunity);
+
+                    // List all user of school to register on group phpfox
+                    $userList = User::where([
+                        ['school_id', '=', $syncSchool->id],
+                        ['active_phpfox', '!=', 0]
+                    ])->get();
+
+                    $t = 0;
+
+                    foreach ($userList as $user) {
+
+                        $userId = $user->id;
+
+                        // DATA PHPFOX_LIKE TABLE
+                        $dataLike = ([
+                            "type_id" => "groups",
+                            "item_id" => $group->page_id,
+                            "user_id" => $user->active_phpfox,
+                            "feed_table" => "feed",
+                            "time_stamp" => Carbon::now()->timestamp
+                        ]);
+
+                        $userLike = LikeUserGroup::create($dataLike);
+
+                        $dataGroup = ([
+                            'user_id' => $userId,
+                            'school_id' => $syncSchool->id,
+                            'group_id_community' => $group->page_id,
+                            'group_id_academy' => 0
+                        ]);
+
+                        $groupCreated = GroupUserEnrollment::create($dataGroup);
+                        $t++;
+                        //$count[$t++] = [$userLike, $groupCreated];
+                    }
                 }
                 $count[$i++] = array("Grupo" => $group->title, "PageText" => $pageText->page_id, "UserCommunity" => $userCommunity->full_name, "UsersEnroller" => $t);
             }
@@ -154,95 +158,103 @@ class SyncGroupComunnityController extends ApiController
 
                 $syncGrade = Group::create($gradeGradeGroup);
 
-                // DATA PHPFOX_PAGES TABLE
-                $dataGradeCommunity = ([
-                    'app_id' => 0,
-                    'view_id' => 0,
-                    'type_id' => 6,
-                    "category_id" => 0,
-                    "user_id" => $teacher->active_phpfox,
-                    "title" => $gradeGroup->Name,
-                    "reg_method" => 2,
-                    "landing_page" => null,
-                    "time_stamp" => Carbon::now()->timestamp,
-                    "image_path" => null,
-                    "is_featured" => 0,
-                    "is_sponsor" => 0,
-                    "image_server_id" => 0,
-                    "total_like" => 1,
-                    "total_dislike" => 0,
-                    "total_comment" => 0,
-                    "privacy" => 0,
-                    "designer_style_id" => 0,
-                    "cover_photo_id" => null,
-                    "cover_photo_position" => null,
-                    "location_latitude" => null,
-                    "location_longitude" => null,
-                    "location_name" => null,
-                    "use_timeline" => 0,
-                    "item_type" => 1
-                ]);
-
-                $groupGradeCommunity = SyncGroupComunnity::create($dataGradeCommunity);
-
-                // DATA PHPFOX_PAGES TABLE
-                $dataText = [
-                    'page_id' => $groupGradeCommunity->page_id,
-                    'text' => null,
-                    'text_parsed' => null
-                ];
-                //database phpfox pages_text
-                $pageText = PhpFoxPageText::create($dataText);
-
-
-                $dataUserCommunity = ([
-                    'profile_page_id' => $groupGradeCommunity->page_id,
-                    'user_group_id' => 2,
-                    'view_id' => 7,
-                    'full_name' => $groupGradeCommunity->title,
-                    'joined' => Carbon::now()->timestamp
-                ]);
-
-                $userCommunity = UserCommunity::create($dataUserCommunity);
-                // List all user of school to register on group phpfox
-                $userList = GradeGroupEnrollment::select('StudentId')->where([
-                    ['GroupId', '=', $gradeGroup->GroupId]
-                ])->get();
-
-                $resultsUserList = User::whereIn('AppUserId', $userList)->get();
-
-
-                $p = 0;
-
-                foreach ($resultsUserList as $users) {
-
-                    $userId = $users->id;
-
-                    // DATA PHPFOX_LIKE TABLE
-                    $dataLike = ([
-                        "type_id" => "groups",
-                        "item_id" => $groupGradeCommunity->page_id,
-                        "user_id" => $users->active_phpfox,
-                        "feed_table" => "feed",
-                        "time_stamp" => Carbon::now()->timestamp
+                if (SyncGroupComunnity::where([['title', '=', $gradeGroup->Name]])) {
+                    $count[$c++] = array('error' => 'El nombre de grupo ya existe');
+                } else {
+                    // DATA PHPFOX_PAGES TABLE
+                    $dataGradeCommunity = ([
+                        'app_id' => 0,
+                        'view_id' => 0,
+                        'type_id' => 6,
+                        "category_id" => 0,
+                        "user_id" => $teacher->active_phpfox,
+                        "title" => $gradeGroup->Name,
+                        "reg_method" => 2,
+                        "landing_page" => null,
+                        "time_stamp" => Carbon::now()->timestamp,
+                        "image_path" => null,
+                        "is_featured" => 0,
+                        "is_sponsor" => 0,
+                        "image_server_id" => 0,
+                        "total_like" => 1,
+                        "total_dislike" => 0,
+                        "total_comment" => 0,
+                        "privacy" => 0,
+                        "designer_style_id" => 0,
+                        "cover_photo_id" => null,
+                        "cover_photo_position" => null,
+                        "location_latitude" => null,
+                        "location_longitude" => null,
+                        "location_name" => null,
+                        "use_timeline" => 0,
+                        "item_type" => 1
                     ]);
 
-                    $userLike = LikeUserGroup::create($dataLike);
+                    $groupGradeCommunity = SyncGroupComunnity::create($dataGradeCommunity);
 
-                    $dataGroup = ([
-                        'user_id' => $userId,
-                        'school_id' => $users->school_id,
-                        'group_id_community' => $groupGradeCommunity->page_id,
-                        'group_id_academy' => 0
+                    // DATA PHPFOX_PAGES TABLE
+                    $dataText = [
+                        'page_id' => $groupGradeCommunity->page_id,
+                        'text' => null,
+                        'text_parsed' => null
+                    ];
+                    //database phpfox pages_text
+                    $pageText = PhpFoxPageText::create($dataText);
+
+                    $dataUserCommunity = ([
+                        'profile_page_id' => $groupGradeCommunity->page_id,
+                        'user_group_id' => 2,
+                        'view_id' => 7,
+                        'full_name' => $groupGradeCommunity->title,
+                        'joined' => Carbon::now()->timestamp
                     ]);
 
-                    $groupCreated = GroupUserEnrollment::create($dataGroup);
-                    $p++;
-                    //$count[$t++] = [$userLike, $groupCreated];
+                    $userCommunity = UserCommunity::create($dataUserCommunity);
+                    // List all user of school to register on group phpfox
+                    $userList = GradeGroupEnrollment::select('StudentId')->where([
+                        ['GroupId', '=', $gradeGroup->GroupId]
+                    ])->get();
+
+                    $resultsUserList = User::whereIn('AppUserId', $userList)->get();
+
+                    $p = 0;
+
+                    foreach ($resultsUserList as $users) {
+
+                        $userId = $users->id;
+
+                        // DATA PHPFOX_LIKE TABLE
+                        $dataLike = ([
+                            "type_id" => "groups",
+                            "item_id" => $groupGradeCommunity->page_id,
+                            "user_id" => $users->active_phpfox,
+                            "feed_table" => "feed",
+                            "time_stamp" => Carbon::now()->timestamp
+                        ]);
+
+                        $userLike = LikeUserGroup::create($dataLike);
+
+                        $dataGroup = ([
+                            'user_id' => $userId,
+                            'school_id' => $users->school_id,
+                            'group_id_community' => $groupGradeCommunity->page_id,
+                            'group_id_academy' => 0
+                        ]);
+
+                        $groupCreated = GroupUserEnrollment::create($dataGroup);
+
+                        $p++;
+                        //$count[$t++] = [$userLike, $groupCreated];
+                    }
                 }
-                $count[$i++] = array("Grupo" => $groupGradeCommunity->title, "PageText" => $pageText->page_id, "UserCommunity" => $userCommunity->full_name, "UsersEnroller" => $p);
+                $count[$c++] = array([
+                    "Grupo" => $groupGradeCommunity->title,
+                    "PageText" => $pageText->page_id,
+                    "UserCommunity" => $userCommunity->full_name,
+                    "UsersEnroller" => $p],
+                    ['Group' => $groupCreated, 'user_like' => $userLike],);
             }
         }
-        return $this->successResponse($userList);
+        return $this->successResponse($count);
     }
 }
